97bcf23a65480d8b0ab1bbcace1054fb768c900c
Max Reble <maxreble@googlemail.com>
2022-05-01 18:48:24 +0200

    fixes tests

--- Muddi.ShiftPlanner.Tests.Unit/Shared/ShiftTests.cs
+++ Muddi.ShiftPlanner.Tests.Unit/Shared/ShiftTests.cs
@@ -17,10 +17,11 @@ public class ShiftTests
 		var shift = new Shift(UserOne, startTime, startTime + timeSpan, DefaultRole);
 		var shift2 = new Shift(UserTwo, startTime, startTime + timeSpan, DefaultRole);
 		var shift3 = new Shift(UserOne, startTime, startTime + timeSpan, DefaultRole);
-		shift.Title.Should().Be(UserOne.Name);
+		shift.Title.Should().StartWith(UserOne.Name.Split(' ')[0]);
 		shift.EndTime.Should().Be(startTime + timeSpan);
 		EqualityChecker.Check(shift, shift2, shift3);
 		(shift == shift3).Should().BeTrue();
+		// ReSharper disable once EqualExpressionComparison
 		(shift != shift).Should().BeFalse();
 		(shift == shift2).Should().BeFalse();
 	}
