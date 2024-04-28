7c73865c67710875510906ce194d9fef1ca6f012
Max Reble <maxreble@googlemail.com>
2024-04-28 15:50:39 +0200

    feat: implement offline mode in backend

---
+++ Muddi.ShiftPlanner.Server.Api/Endpoints/Seasons/GetOfflineCacheTimeEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Muddi.ShiftPlanner.Server.Database.Contexts;
+
+namespace Muddi.ShiftPlanner.Server.Api.Endpoints.Seasons;
+
+public class GetOfflineCacheTimeEndpoint : CrudGetEndpoint<SeasonOfflineCacheTimeResponse>
+{
+	public GetOfflineCacheTimeEndpoint(ShiftPlannerContext database) : base(database)
+	{
+	}
+
+	protected override void CrudConfigure()
+	{
+		Get("/seasons/{id:guid}/cache-time");
+	}
+
+	public override Task<SeasonOfflineCacheTimeResponse?> CrudExecuteAsync(Guid id, CancellationToken ct)
+	{
+		return Task.FromResult(new SeasonOfflineCacheTimeResponse { LastCacheTime = Database.GetLastCacheTime() })!;
+	}
+}
+
+public class SeasonOfflineCacheTimeResponse
+{
+	public DateTime LastCacheTime { get; init; }
+}
