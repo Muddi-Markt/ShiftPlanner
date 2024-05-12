799989abe9dc1f792ccdcc96af7df86b72201746
Max Reble <maxreble@googlemail.com>
2024-05-12 19:55:57 +0200

    feat: add deployment docker compose

--- Muddi.ShiftPlanner.Server.Database/Extensions/ServiceCollectionExtensions.cs
+++ Muddi.ShiftPlanner.Server.Database/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Data.Common;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Muddi.ShiftPlanner.Server.Database.Contexts;
+using Npgsql;
 
 namespace Muddi.ShiftPlanner.Server.Database.Extensions;
 
@@ -9,8 +12,27 @@ public static class ServiceCollectionExtensions
 {
 	public static void AddMuddiShiftPlannerContext(this IServiceCollection services, IConfiguration configuration)
 	{
+		if (configuration.GetConnectionString("ShiftPlannerDb") is not string connectionString)
+		{
+			var applicationName = Assembly.GetEntryAssembly()!.GetName().Name;
+			var dbConfig = configuration.GetRequiredSection("Database");
+			var builder = new NpgsqlConnectionStringBuilder
+			{
+				BrowsableConnectionString = false,
+				ConnectionString = null,
+				Host = dbConfig["Host"],
+				Port = dbConfig.GetValue("Port", 5432),
+				Database = dbConfig["Name"],
+				Username = dbConfig["User"],
+				Password = dbConfig["Password"],
+				ApplicationName = applicationName,
+				IncludeErrorDetail = dbConfig.GetValue("IncludeErrorDetail", false)
+			};
+			connectionString = builder.ConnectionString;
+		}
+
 		services.AddNpgsql<ShiftPlannerContext>(
-			configuration.GetConnectionString("ShiftPlannerDb"),
+			connectionString,
 			null,
 			a => a.UseSnakeCaseNamingConvention());
 	}
