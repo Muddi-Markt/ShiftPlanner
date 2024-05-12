dd404f1f6be763c1d0ac5859a80098981f32026c
Max Reble <maxreble@googlemail.com>
2024-05-12 19:54:12 +0200

    refactor: remove build files

--- Docker/build-alerting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd ..
-current_timestamp=`date +%s`
-#sed -i "s/cacheVersion\ =\ '.*'/cacheVersion = '${current_timestamp}'/g" Muddi.ShiftPlanner.Client/wwwroot/service-worker.published.js
-docker build -f Muddi.ShiftPlanner.Services.Alerting/Dockerfile -t hse-server01:5000/muddi/shiftplanner/alerting-service -t muddi/shiftplanner/alerting-service . --network=host
-docker push hse-server01:5000/muddi/shiftplanner/alerting-service
-#docker save -o muddi-client.tar hse-server01:5000/muddi/shiftplanner/client
