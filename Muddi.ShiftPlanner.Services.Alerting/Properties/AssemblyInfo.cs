34b6a514271eb295a0dd8262eb12477e02340897
Max Reble <maxreble@googlemail.com>
2024-05-15 22:28:52 +0200

    build: fix workflow

---
+++ Muddi.ShiftPlanner.Services.Alerting/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("MUDDI Markt e.V.")]
+[assembly: AssemblyCopyright("MUDDI Markt e.V., Max Reble")]
+[assembly: AssemblyTitle("MUDDIs ShiftPlanner Alerting Service")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyInformationalVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
