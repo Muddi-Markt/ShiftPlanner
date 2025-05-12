c32396b8993f2a020c64299222775c392c9d3571
Max Reble <maxreble@googlemail.com>
2025-05-12 17:18:49 +0200

    feat: add admin only flag database migration

---
+++ Muddi.ShiftPlanner.Server.Database/Migrations/20250512150651_AdminOnlyLocationTypes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Muddi.ShiftPlanner.Server.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminOnlyLocationTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "admin_only",
+                table: "shift_location_types",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "admin_only",
+                table: "shift_location_types");
+        }
+    }
+}
