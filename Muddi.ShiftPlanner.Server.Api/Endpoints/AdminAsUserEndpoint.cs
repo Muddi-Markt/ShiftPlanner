d7a718189ed0eab9106b92cf40e36df1e957f788
Max Reble <maxreble@googlemail.com>
2025-12-22 11:46:44 +0100

    feat: add admin debug endpoints

---
+++ Muddi.ShiftPlanner.Server.Api/Endpoints/AdminAsUserEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+
+namespace Muddi.ShiftPlanner.Server.Api.Endpoints;
+
+public class AdminAsUserEndpoint : EndpointWithoutRequest
+{
+	public override void Configure()
+	{
+		Get("/admin/as-user");
+	}
+
+	public override Task HandleAsync(CancellationToken ct)
+	{
+		var claims = User.Claims.ToLookup(x => x.Type, x => x.Value);
+		object res = claims.Contains("roles")
+			? new { roles = claims["roles"] }
+			: claims;
+		return Send.OkAsync(res, cancellation: ct);
+	}
+}
