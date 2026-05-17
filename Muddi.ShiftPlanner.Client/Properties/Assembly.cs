2847a7a85e4c7b4be309c59cec067a34d9375b48
Max Reble <39700092+maxreb@users.noreply.github.com>
2026-05-17 11:53:51 +0200

    build: allow manual Docker image publishing with short commit hash (#70)
    * build: allow manual dispatch of building docker images with git hash
    
    * fix: use correct version for manual dispatch

--- Muddi.ShiftPlanner.Client/Properties/Assembly.cs
+++ Muddi.ShiftPlanner.Client/Properties/Assembly.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCopyright("MUDDI Markt e.V., Max Reble")]
 [assembly: AssemblyTitle("MUDDIs ShiftPlanner Client")]
 [assembly: AssemblyVersion("1.4")]
-[assembly: AssemblyInformationalVersion("1.4")]
-[assembly: AssemblyFileVersion("1.4")]
+[assembly: AssemblyInformationalVersion("1.4-dev")]
+[assembly: AssemblyFileVersion("1.4-dev")]
